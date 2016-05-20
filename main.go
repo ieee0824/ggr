d36fdd2eea88ea8d073ad71265ead157abea3867
Ryo Matsumoto <ryo.mats302@gmail.com>
2016-05-20 16:12:51 +0900

    Add command line option -i, -s, -n

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import(
 	"flag"
 	"net/url"
 	"strings"
+    "os"
 
 	"github.com/toqueteos/webbrowser"
 )
@@ -16,6 +17,7 @@ type Ggr struct {
     ie string
     oe string
     hl string
+    tbm string
 }
 
 func NewGgr() Ggr{
@@ -24,7 +26,7 @@ func NewGgr() Ggr{
         ie : "utf-8",
         oe : "utf-8",
         hl : "ja",
-        tbm : ""
+        tbm : "",
     }
     return g
 }
@@ -35,6 +37,7 @@ func (p Ggr) getSearchURL() string {
 	values.Add("ie", p.ie)
 	values.Add("oe", p.oe)
 	values.Add("hl", p.hl)
+    values.Add("tbm", p.tbm)
 	u := url.URL{}
 	u.Scheme = "https"
 	u.Host = "www.google.co.jp"
@@ -44,9 +47,28 @@ func (p Ggr) getSearchURL() string {
 }
 
 func main() {
-	flag.Parse()
-	args := flag.Args()
+    var (
+        imageFlag bool
+        newsFlag bool
+        shopFlag bool
+    )
+    
+    f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+    f.BoolVar(&imageFlag, "i", false, "image flag")
+    f.BoolVar(&newsFlag, "n", false, "new flag")
+    f.BoolVar(&shopFlag, "s", false, "shop flag")
+    
+	f.Parse(os.Args[1:])
+	args := f.Args()
+    
     g := NewGgr()
     g.q = strings.Join(args, " ")
+    if imageFlag {
+        g.tbm = "isch"
+    } else if newsFlag {
+        g.tbm = "nws"
+    } else if shopFlag {
+        g.tbm = "shop"
+    }
 	webbrowser.Open(g.getSearchURL())
 }
