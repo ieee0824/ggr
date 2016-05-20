05f11f6896e1c65bd233ac1618564a289248c4d4
Ryo Matsumoto <ryo.mats302@gmail.com>
2016-05-20 16:00:01 +0900

    Refactoring

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+import(
 	"flag"
 	"net/url"
 	"strings"
@@ -11,19 +11,42 @@ import (
 // sample url
 // https://www.google.co.jp/search?q=test&ie=utf-8&oe=utf-8&hl=ja
 
-func main() {
-	flag.Parse()
+type Ggr struct {
+    q string
+    ie string
+    oe string
+    hl string
+}
+
+func NewGgr() Ggr{
+    g := Ggr{
+        q : "",
+        ie : "utf-8",
+        oe : "utf-8",
+        hl : "ja",
+        tbm : ""
+    }
+    return g
+}
+
+func (p Ggr) getSearchURL() string {
 	values := url.Values{}
-	args := flag.Args()
-	q := strings.Join(args, " ")
-	values.Add("q", q)
-	values.Add("ie", "utf-8")
-	values.Add("oe", "utf-8")
-	values.Add("hl", "ja")
+	values.Add("q", p.q)
+	values.Add("ie", p.ie)
+	values.Add("oe", p.oe)
+	values.Add("hl", p.hl)
 	u := url.URL{}
 	u.Scheme = "https"
 	u.Host = "www.google.co.jp"
 	u.Path = "search"
 	u.RawQuery = values.Encode()
-	webbrowser.Open(u.String())
+    return u.String()
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+    g := NewGgr()
+    g.q = strings.Join(args, " ")
+	webbrowser.Open(g.getSearchURL())
 }
